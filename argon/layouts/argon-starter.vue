56be3db999b98e965b735445b16d90337d248145
gstrainovic <g.strainovic@gmail.com>
2019-02-05 14:57:10 +0000

    on work

--- argon/layouts/argon-starter.vue
+++ argon/layouts/argon-starter.vue
@@ -1,117 +1,19 @@
-<template>
-  <div>
-    <header class="header-global">
-        <base-nav class="navbar-main" transparent type="" effect="light" expand>
-            <nuxt-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <!-- <img src="/argon/img/brand/white.png"> -->
-                Strainovic IT
-            </nuxt-link>
-
-
-            <div class="row" slot="content-header" slot-scope="{closeMenu}">
-
-
-              <div class="col-6 collapse-brand">
-                    <nuxt-link to="/">
-                Strainovic IT
-                      <!-- <img src="/argon/img/brand/blue.png"> -->
-                    </nuxt-link>
-                </div>
-                <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu"></close-button>
-                </div>
-            </div>
-            <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
- 
-             <li class="nav-item">
-              <nuxt-link class="nav-link" to="#">Dienstleistungen</nuxt-link>
-            </li>
-
-            <li class="nav-item dropdown">
-                    <a href="#" class="nav-link" data-toggle="dropdown" role="button">
-                        <i class="ni ni-ui-04 d-lg-none"></i>
-                        <span class="nav-link-inner--text">Components</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-xl">
-                        <div class="dropdown-menu-inner">
-                            <a href="https://demos.creative-tim.com/argon-design-system/docs/getting-started/overview.html"
-                               class="media d-flex align-items-center">
-                                <div class="icon icon-shape bg-gradient-primary rounded-circle text-white">
-                                    <i class="ni ni- spaceship"></i>
-                                </div>
-                                <div class="media-body ml-3">
-                                    <h6 class="heading text-primary mb-md-1">Getting started</h6>
-                                    <p class="description d-none d-md-inline-block mb-0">Get started with Bootstrap, the
-                                        world's most popular framework for building responsive sites.</p>
-                                </div>
-                            </a>
-                            <a href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html"
-                               class="media d-flex align-items-center">
-                                <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                                    <i class="ni ni-ui-04"></i>
-                                </div>
-                                <div class="media-body ml-3">
-                                    <h5 class="heading text-warning mb-md-1">Components</h5>
-                                    <p class="description d-none d-md-inline-block mb-0">Learn how to use Argon
-                                        compiling Scss, change brand colors and more.</p>
-                                </div>
-                            </a>
-                        </div>
                     </div>
                 </li>
-                <li class="nav-item dropdown">
+                 <li class="nav-item dropdown">
                     <a href="#" class="nav-link" data-toggle="dropdown" role="button">
-                        <i class="ni ni-collection d-lg-none"></i>
-                        <span class="nav-link-inner--text">Examples</span>
                     </a>
                     <div class="dropdown-menu">
-                        <nuxt-link :to="{name: 'argon-demo-landing'}" class="dropdown-item">Landing</nuxt-link>
-                        <nuxt-link :to="{name: 'argon-demo-profile'}" class="dropdown-item">Profile</nuxt-link>
-                        <nuxt-link :to="{name: 'argon-demo-login'}" class="dropdown-item">Login</nuxt-link>
-                        <nuxt-link :to="{name: 'argon-demo-register'}" class="dropdown-item">Register</nuxt-link>
-                    </div>
+                   </div>
+                </li>
+                  <li class="nav-item dropdown">
+                    <a href="#" class="nav-link" data-toggle="dropdown" role="button">
+                    </a>
+                    <div class="dropdown-menu">
+                   </div>
                 </li>
             </ul>
-            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.facebook.com/creativetim" target="_blank"
-                       data-toggle="tooltip" title="Like us on Facebook">
-                        <i class="fa fa-facebook-square"></i>
-                        <span class="nav-link-inner--text d-lg-none">Facebook</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.instagram.com/creativetimofficial"
-                       target="_blank" data-toggle="tooltip" title="Follow us on Instagram">
-                        <i class="fa fa-instagram"></i>
-                        <span class="nav-link-inner--text d-lg-none">Instagram</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://twitter.com/creativetim" target="_blank"
-                       data-toggle="tooltip" title="Follow us on Twitter">
-                        <i class="fa fa-twitter-square"></i>
-                        <span class="nav-link-inner--text d-lg-none">Twitter</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://github.com/creativetimofficial/vue-argon-design-system"
-                       target="_blank" data-toggle="tooltip" title="Star us on Github">
-                        <i class="fa fa-github"></i>
-                        <span class="nav-link-inner--text d-lg-none">Github</span>
-                    </a>
-                </li>
-                <li class="nav-item d-none d-lg-block ml-lg-4">
-                    <a href="https://www.creative-tim.com/product/vue-argon-design-system" target="_blank"
-                       class="btn btn-neutral btn-icon">
-                <span class="btn-inner--icon">
-                  <i class="fa fa-cloud-download mr-2"></i>
-                </span>
-                        <span class="nav-link-inner--text">Download</span>
-                    </a>
-                </li>
-            </ul>
-        </base-nav>
+       </base-nav>
     </header>
     <main>
         <nuxt />
@@ -138,32 +40,44 @@
         <div class="container">
             <div class="row row-grid align-items-center my-md">
                 <div class="col-lg-6">
-                    <h3 class="text-primary font-weight-light mb-2">Thank you for supporting us!</h3>
-                    <h4 class="mb-0 font-weight-light">Let's get in touch on any of these platforms.</h4>
+                    <h3 class="text-primary font-weight-light mb-2">Vielen Dank für deine Unterstützung!</h3>
+                    <h4 class="mb-0 font-weight-light">Folge mir auf einer dieser Plattformen.</h4>
                 </div>
                 <div class="col-lg-6 text-lg-center btn-wrapper">
-                    <a target="_blank" href="https://twitter.com/creativetim"
+                    <a target="_blank" href="https://twitter.com/Strainovic_IT"
                        class="btn btn-neutral btn-icon-only btn-twitter btn-round btn-lg" data-toggle="tooltip"
-                       data-original-title="Follow us">
+                       title="Folge mir auf Twitter">
                         <i class="fa fa-twitter"></i>
                     </a>
-                    <a target="_blank" href="https://www.facebook.com/creativetim"
+                    <a target="_blank" href="https://www.facebook.com/StrainovicIT/"
                        class="btn btn-neutral btn-icon-only btn-facebook btn-round btn-lg" data-toggle="tooltip"
-                       data-original-title="Like us">
+                       title="Like mich auf Facebook">
                         <i class="fa fa-facebook-square"></i>
                     </a>
-                    <a target="_blank" href="https://dribbble.com/creativetim"
-                       class="btn btn-neutral btn-icon-only btn-dribbble btn-lg btn-round" data-toggle="tooltip"
-                       data-original-title="Follow us">
-                        <i class="fa fa-dribbble"></i>
+                    <a target="_blank" href="https://www.instagram.com/strainovic_it/"
+                       class="btn btn-neutral btn-icon-only btn-instagram btn-lg btn-round" data-toggle="tooltip"
+                       title="Folge mir auf Instagram">
+                        <i class="fa fa-instagram"></i>
                     </a>
-                    <a target="_blank" href="https://github.com/creativetimofficial"
+                    <a target="_blank" href="https://github.com/gstrainovic"
                        class="btn btn-neutral btn-icon-only btn-github btn-round btn-lg" data-toggle="tooltip"
-                       data-original-title="Star on Github">
+                       title="Spendiere mir einen Stern auf Github">
                         <i class="fa fa-github"></i>
+                    </a>
+                      <a target="_blank" href="https://ch.linkedin.com/in/goran-strainovic"
+                       class="btn btn-neutral btn-icon-only btn-linkedin btn-round btn-lg" data-toggle="tooltip"
+                       title="Folge mir auf LinkedIn">
+                        <i class="fa fa-linkedin"></i>
+                    </a>
+                     <a target="_blank" href="https://www.xing.com/profile/Goran_Strainovic"
+                       class="btn btn-neutral btn-icon-only btn-xing btn-round btn-lg" data-toggle="tooltip"
+                       title="Folge mir auf Xing">
+                        <i class="fa fa-xing"></i>
                     </a>
                 </div>
             </div>
+
+          </ul>
             <hr>
             <div class="row align-items-center justify-content-md-between">
                 <div class="col-md-6">
@@ -209,5 +123,5 @@ export default {
 </script>
 
 <style>
-    
+
 </style>
