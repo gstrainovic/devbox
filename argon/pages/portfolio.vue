7233fa6c9f3f85b26c1f7a5cc0af478929190b82
gstrainovic <g.strainovic@gmail.com>
2019-02-24 23:06:25 +0000

    portfolio

--- argon/pages/portfolio.vue
+++ argon/pages/portfolio.vue
@@ -35,9 +35,9 @@
                       <div class="row">
                           <div class="col-md-6 mb-5 mb-md-0 mt-5">
                               <div class="card card-lift--hover shadow">
-                                  <nuxt-link to="/landing" title="Landing Page">
+                                  <a href="http://ddp-transporte.ch" target="_blank">
                                       <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-ddp.jpg'" class="card-img-top">
-                                  </nuxt-link>
+                                  </a>
                                   <div class="card-body">
                                      <table class="table">
                                         <tbody>
@@ -68,33 +68,202 @@
                          </div>
                           <div class="col-md-6 mb-5 mb-lg-0 mt-5">
                               <div class="card card-lift--hover shadow border-0">
-                                  <nuxt-link :to="{name: 'argon-demo-profile'}" title="Profile Page">
+                                  <a href="http://buegeln.services" target="_blank">
                                       <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-bs.jpg'" class="card-img">
-                                  </nuxt-link>
+                                  </a>
+                                  <div class="card-body">
+                                     <table class="table">
+                                        <tbody>
+                                            <tr>
+                                              <th>Projekt</th>
+                                              <td>Webseite</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Branche</th>
+                                              <td>Bügelservice</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Firmenort</th>
+                                              <td>CH-9323 Steinach SG, Kanton St.Gallen, Schweiz</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Technologie</th>
+                                              <td>Jekyll</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Link</th>
+                                              <td><a href="http://buegeln.services" target="_blank">http://buegeln.services</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                  </div>
+                              </div>
+                            </div>
+                          <div class="col-md-6 mb-5 mb-lg-0 mt-5">
+                              <div class="card card-lift--hover shadow border-0">
+                                  <a href="http://grav.gost.pw/mijajlovic.ch" target="_blank">
+                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-mij.jpg'" class="card-img">
+                                  </a>
+                                  <div class="card-body">
+                                     <table class="table">
+                                        <tbody>
+                                            <tr>
+                                              <th>Projekt</th>
+                                              <td>Webseite</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Branche</th>
+                                              <td>Spezialist für Boden</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Firmenort</th>
+                                              <td>CH-9320 Gossau SG, Kanton St.Gallen, Schweiz</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Technologie</th>
+                                              <td>Jekyll</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Link</th>
+                                              <td><a href="http://grav.gost.pw/mijajlovic.ch" target="_blank">http://grav.gost.pw/mijajlovic.ch</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                  </div>
                               </div>
                           </div>
                           <div class="col-md-6 mb-5 mb-lg-0 mt-5">
                               <div class="card card-lift--hover shadow border-0">
-                                  <nuxt-link :to="{name: 'argon-demo-profile'}" title="Profile Page">
-                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-bs.jpg'" class="card-img">
-                                  </nuxt-link>
+                                  <a href="http://grav.gost.pw/susanne" target="_blank">
+                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-sb.jpg'" class="card-img">
+                                  </a>
+                                 <div class="card-body">
+                                     <table class="table">
+                                        <tbody>
+                                            <tr>
+                                              <th>Projekt</th>
+                                              <td>Webseite</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Branche</th>
+                                              <td>Pferdegestü. Coaching und Reiten</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Firmenort</th>
+                                              <td>CH-9305 Waldkirch SG, Kanton St.Gallen, Schweiz</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Technologie</th>
+                                              <td>Grav CMS</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Link</th>
+                                              <td><a href="http://grav.gost.pw/susanne" target="_blank">http://grav.gost.pw/susanne</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                  </div>
                               </div>
                           </div>
                           <div class="col-md-6 mb-5 mb-lg-0 mt-5">
                               <div class="card card-lift--hover shadow border-0">
-                                  <nuxt-link :to="{name: 'argon-demo-profile'}" title="Profile Page">
-                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-bs.jpg'" class="card-img">
-                                  </nuxt-link>
+                                  <a href="http://strainovic-it.ch" target="_blank">
+                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-sit.jpg'" class="card-img">
+                                  </a>
+                                  <div class="card-body">
+                                     <table class="table">
+                                        <tbody>
+                                            <tr>
+                                              <th>Projekt</th>
+                                              <td>Webseite</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Branche</th>
+                                              <td>IT Dienstleistungen</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Firmenort</th>
+                                              <td>CH-9323 Steinach SG, Kanton St.Gallen, Schweiz</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Technologie</th>
+                                              <td>WordPress</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Link</th>
+                                              <td><a href="http://strainovic-it.ch" target="_blank">http://strainovic-it.ch</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                  </div>
                               </div>
                           </div>
                           <div class="col-md-6 mb-5 mb-lg-0 mt-5">
                               <div class="card card-lift--hover shadow border-0">
-                                  <nuxt-link :to="{name: 'argon-demo-profile'}" title="Profile Page">
-                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-bs.jpg'" class="card-img">
-                                  </nuxt-link>
+                                  <a href="https://gstrainovic.github.io/gost.pw" target="_blank">
+                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-cv.jpg'" class="card-img">
+                                  </a>
+                                  <div class="card-body">
+                                     <table class="table">
+                                        <tbody>
+                                            <tr>
+                                              <th>Projekt</th>
+                                              <td>Webseite</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Branche</th>
+                                              <td>CV / Lebenslauf</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Firmenort</th>
+                                              <td>CH-9323 Steinach SG, Kanton St.Gallen, Schweiz</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Technologie</th>
+                                              <td>Jekyll</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Link</th>
+                                              <td><a href="https://gstrainovic.github.io/gost.pw" target="_blank">http://strainovic-it.ch</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                  </div>
                               </div>
                           </div>
-                         </div>
+                            <div class="col-md-6 mb-5 mb-lg-0 mt-5">
+                              <div class="card card-lift--hover shadow border-0">
+                                  <a href="https://gstrainovic.github.io/gost.pw" target="_blank">
+                                      <img v-lazy="'/portfolio-responsive-webdesign/ResponsiveWebDesign-cv.jpg'" class="card-img">
+                                  </a>
+                                  <div class="card-body">
+                                     <table class="table">
+                                        <tbody>
+                                            <tr>
+                                              <th>Projekt</th>
+                                              <td>Webseite</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Branche</th>
+                                              <td>IT Dienstleistungen</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Firmenort</th>
+                                              <td>CH-9323 Steinach SG, Kanton St.Gallen, Schweiz</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Technologie</th>
+                                              <td>Vue, Nuxt, Argon, Bootstrap</td>
+                                            </tr>
+                                            <tr>
+                                              <th>Link</th>
+                                              <td><a href="https://gstrainovic.github.io/gost.pw" target="_blank">http://strainovic-it.ch</a></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                  </div>
+                              </div>
+                          </div>                      </div>
                   </div>
               </div>
             </div>
