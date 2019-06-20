08c7e938b8e351dd7a8e7a3b710b0f2865df5aab
gstrainovic <g.strainovic@gmail.com>
2019-06-20 23:15:16 +0200

    -

--- argon/.eslintrc.js
+++ argon/.eslintrc.js
@@ -1,23 +1,27 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-    'prettier/vue'
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
+    
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
   rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": ["error", {
+     "endOfLine":"auto"
+   }],
+  },
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
-}
+};
